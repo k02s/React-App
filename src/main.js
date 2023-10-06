51c696600356d8ebdd967b1cf9ea51d219b49ed9
k02s <khsaleh022@gmail.com>
2023-10-06 21:13:01 +0300

    lab 06

--- src/main.js
+++ src/main.js
@@ -3,40 +3,70 @@ import data from './data.json';
 import CardFood from './card';
 import { useState } from 'react';
 import './style.css';
+import { useEffect } from 'react';
 
 function Main() {
 
-  let [product, setProduct] = useState(data)
+  let [items, setMeals] = useState([]);
 
-  function handleSearch(event){
-    event.preventDefault()
-    let searchedValue = event.target.search.value 
-    let fileredProducts = data.filter(function(item){ return item.title.toLowerCase().includes(searchedValue.toLowerCase()) })
-    setProduct(fileredProducts)
+  // fetch api data
+  // fetch take time so we use await and put async for function & fetch take to parameters url and options 
+  // the data returns from API should be in a fuction cause it returns promise
+  // we need to use useState cause the data come from API take a time so we need an inital state 
+  async function getData() {
+
+    let url = "https://www.themealdb.com/api/json/v1/1/search.php?f=m"
+    let response = await fetch(url);
+    let result = await response.json();
+
+    setMeals(result.meals);
+
+  }
+  //  useEffect build-in function we need just import to use it 
+  // it take two parameterd the first function and the second is array 
+  // the function will performd based on array so i can let function change based on spesific variable
+  useEffect(function () {
+    getData()
+  }, [])
+
+  // search bar functionality
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let searchedValue = event.target.search.value
+
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=` + searchedValue);
+    let result = await response.json();
+
+    setMeals(result.meals)
+    let filteredMeals = result.meals.filter(function (item) { return item.strMeal.toLowerCase().includes(searchedValue.toLowerCase()) })
+    setMeals(filteredMeals);
+
+
   }
 
-  return ( 
+  return (
     <>
-    <form onSubmit={handleSearch} id="search-bar">
+      {/* search bar */}
+      <form id="search-bar" onSubmit={handleSubmit}>
         <input type="text" name="search" placeholder='Search'></input>
         <button id="search-btn" type="submit">Search</button>
-    </form>
-    <div id = "card-style">
-    {/* map = store data in a new array */}
-    {product.length === 0 ? (
-          <p style = {{marginLeft: "550px"}}> No search results found.</p>
-        ) :(
-    product.map(function(item){
-        
-        return (
-            <CardFood  image = {item.image_url} title = {item.title} description = {item.description}/>
-        )
-    
-    }) // for function 
-        )
-    }
-        
-    </div>
+      </form>
+
+      {/* card to show the recipes in card */}
+      <div id="card-style">
+        {/* map = store data in a new array and iterate on array so meal here is an array*/}
+        {
+          items && items.length !== 0 ? items.map(function (item) {
+            return (
+              <>
+                <CardFood key={item.idMeal} image={item.strMealThumb} title={item.strMeal} description={item.strInstructions} category={item.strCategory} FavoriteView={true}/>
+              </>
+            )
+          }
+          ) : <h3>No search results</h3>
+        }
+      </div>
+
     </>
   );
 }
--- src/recipes.js
+++ src/recipes.js
@@ -1,42 +1,82 @@
 import CardFood from './card';
 import data from './data.json';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
 
 function Recipes() {
-    let [selectedCategory, setSelectedCategory] = useState('All');
 
-    function chageCategory(event){
-        setSelectedCategory(event.target.value);
+    let [items, setItems] = useState([]);
+    let [categories, setCategories] = useState([]);
+    let [loading, setLoading] = useState(true);
+
+    // render meals in this page by first letter which is c 
+    async function getMeals() {
+
+        let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=c`);
+        let result = await response.json();
+        setItems(result.meals);
+        setLoading(false)
     }
 
-    let filteredRecipes = data;
-    if (selectedCategory !== 'All') {
-        filteredRecipes = data.filter(function (item) {
-            return item.category === selectedCategory;
-        });
+    //  render the options of select from api
+    async function fetchCategories() {
+        let response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+        let result = await response.json();
+        setCategories(result.categories);
     }
-    
+
+    //  change category based on api 
+    async function fetchMealsByCategory(category) {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=` + category);
+        const result = await response.json();
+        setItems(result.meals);
+    }
+
+
+
+    useEffect(function () {
+        fetchCategories();
+        fetchMealsByCategory();
+        getMeals('all');
+    }, [])
+
+    async function handleChange(event) {
+        const changedValue = event.target.value;
+        if (changedValue === "all") {
+            await getMeals('all');
+        } else {
+            await fetchMealsByCategory(changedValue);
+        }
+    };
+
     return (
         <>
-            {/* drop down button */}
-            <select id="select-category" onChange={chageCategory} value={selectedCategory}>
-                <option value="All">All</option>
-                <option value="Main Course">Main Course</option>
-                <option value="Desserts">Dessert</option>
-                <option value="Salads">Salads</option>
-                <option value="Appetizers">Appetizers</option>
+            {/* drop down menu */}
+            <select id="select-category" onChange={handleChange} >
+                <option value="all">All</option>
+                {/* show options */}
+                {categories.map((category) => (
+                    <option key={category.strCategory} value={category.strCategory}> {category.strCategory} </option>
+                ))}
             </select>
-        
+
 
             {/* show cards */}
             <div id="card-style">
-                {filteredRecipes.map(function (item) {
-                    return (
-                        <CardFood image={item.image_url} title={item.title} description={item.description} />
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    items && items.length !== 0 ? (items.map((item) => (
+
+                        <>
+                            <CardFood key={item.idMeal} image={item.strMealThumb} title={item.strMeal} description={item.strInstructions} FavoriteView={true}
+                            />
+                        </>
+                    ))
+                    ) : (
+                        <h3>No search results</h3>
                     )
-                }) // for function
-                }
+                )}
             </div>
 
         </>
